e42e9a5b9e0246303041756445dbb7f31f94227f
vanhadson <vanhadson@gmail.com>
2025-06-05 17:59:38 +0300

    changed app.js (adding templates for event listeners)

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ const selectors = {
 	sliderValue: document.querySelector('.value'),
 	input: document.querySelector('input[type="text"]')
 }
+
+document.querySelector('#app').addEventListener('click', event => {
+	switch (event.target.dataset.jsSelector) {
+		// Event listener for copy
+		case selectors.copy:
+
+			break;
+
+		// Event listeners for checkboxes
+		case selectors.checkbox:
+
+			break;
+
+		// Event listeners for slider
+		case selectors.slider:
+
+			break;
+
+		// Event listener for generate button
+		case selectors.button:
+
+			break;
+	}
+})
